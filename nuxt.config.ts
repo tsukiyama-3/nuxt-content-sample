8dd95829b586fe8048119ec59894b3155ce78152
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-02-24 22:06:25 +0900

    add: tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  modules: ['@nuxt/eslint', '@nuxt/content'],
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", '@nuxt/content'],
+  css: ['~/assets/css/tailwind.css'],
+  compatibilityDate: '2024-11-01',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   eslint: {
     config: {
       stylistic: true,
